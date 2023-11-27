89b638f73a7c2bcf3c615f51d460f6f354447901
danenherdi <danendra.herdiansyah@ui.ac.id>
2023-11-27 13:27:13 +0700

    Initiate TE2

---
+++ TE2-CODE/src/Main.java
@@ -0,0 +1,91 @@
+public class Main {
+
+    public static void doSort(int[] arr){
+//        int[] copiedArr = new int[arr.length];
+//        System.arraycopy(arr, 0, copiedArr, 0, arr.length);
+//        doRadixSort(arr);
+//        doPeekSort(copiedArr);
+    }
+
+    // Radix sort performance
+    public static void doRadixSort(int[] arr){
+//        long radixSortStartTime = System.currentTimeMillis();
+//        Radixsort.radixSort(arr);
+//        long radixSortEndTime = System.currentTimeMillis();
+//        long memoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+//        long radixSortResultTime = radixSortEndTime - radixSortStartTime;
+//
+//        System.out.println("Penggunaan memori Radix Sort: " + memoryUsed + " bytes");
+//        System.out.println("Waktu eksekusi Radix Sort: " + radixSortResultTime + " ms");
+//        System.out.println();
+    }
+
+    // Peek sort performance
+    public static void doPeekSort(int[] arr){
+//        long peekSortStartTime = System.currentTimeMillis();
+//        Peeksort.peekSort(arr, 0, arr.length-1, 0, arr.length-1, new int[arr.length]);
+//        long peekSortEndTime = System.currentTimeMillis();
+//        long memoryUsed = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+//        long peekSortResultTime = peekSortEndTime - peekSortStartTime;
+//
+//        System.out.println("Penggunaan memori Peek Sort: " + memoryUsed + " bytes");
+//        System.out.println("Waktu eksekusi Peek Sort: " + peekSortResultTime + " ms");
+//        System.out.println();
+    }
+
+    public static void main(String[] args) {
+//        // create sorted array
+//        int[] sortedArr1000 = Util.createSortedData(1000);
+//        int[] sortedArr10000 = Util.createSortedData(10000);
+//        int[] sortedArr100000 = Util.createSortedData(100000);
+//
+//        // create random array
+//        int[] randomArr1000 = Util.createRandomData(1000);
+//        int[] randomArr10000 = Util.createRandomData(10000);
+//        int[] randomArr100000 = Util.createRandomData(100000);
+//
+//        // create reversed array
+//        int[] reversedArr1000 = Util.createReversedList(1000);
+//        int[] reversedArr10000 = Util.createReversedList(10000);
+//        int[] reversedArr100000 = Util.createReversedList(100000);
+//
+//        System.out.println("==================================================");
+//        System.out.println("Data Array Sorted");
+//        System.out.println("==================================================");
+//        System.out.println("Data 1000 Angka");
+//        doSort(sortedArr1000);
+//        System.out.println();
+//        System.out.println("Data 10000 Angka");
+//        doSort(sortedArr10000);
+//        System.out.println();
+//        System.out.println("Data 100000 Angka");
+//        doSort(sortedArr100000);
+//        System.out.println();
+//
+//        System.out.println("==================================================");
+//        System.out.println("Data Array Random");
+//        System.out.println("==================================================");
+//        System.out.println("Data 1000 Angka");
+//        doSort(randomArr1000);
+//        System.out.println();
+//        System.out.println("Data 10000 Angka");
+//        doSort(randomArr10000);
+//        System.out.println();
+//        System.out.println("Data 100000 Angka");
+//        doSort(randomArr100000);
+//        System.out.println();
+//
+//        System.out.println("==================================================");
+//        System.out.println("Data Array Reversed");
+//        System.out.println("==================================================");
+//        System.out.println("Data 1000 Angka");
+//        doSort(reversedArr1000);
+//        System.out.println();
+//        System.out.println("Data 10000 Angka");
+//        doSort(reversedArr10000);
+//        System.out.println();
+//        System.out.println("Data 100000 Angka");
+//        doSort(reversedArr100000);
+
+    }
+}
---
+++ TE2-CODE/src/Util.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class Util {
+    public Util() {
+    }
+
+//    static int[] createSortedData(int size) {
+//        int[] sortedArray = new int[size];
+//
+//        for(int i = 0; i < size; ++i) {
+//            sortedArray[i] = i + 1;
+//        }
+//
+//        return sortedArray;
+//    }
+//
+//    static int[] createRandomData(int size) {
+//        int[] randomArray = new int[size];
+//
+//        for(int i = 0; i < size; ++i) {
+//            randomArray[i] = i + 1;
+//        }
+//
+//        shuffleArray(randomArray);
+//        return randomArray;
+//    }
+//
+//    static int[] createReversedList(int size) {
+//        int[] reversedArray = new int[size];
+//
+//        for(int i = 0; i < size; ++i) {
+//            reversedArray[i] = size - i;
+//        }
+//
+//        return reversedArray;
+//    }
+//
+//    private static void shuffleArray(int[] array) {
+//        Random rand = new Random(System.currentTimeMillis());
+//
+//        for(int i = array.length - 1; i > 0; --i) {
+//            int index = rand.nextInt(i + 1);
+//            int temp = array[index];
+//            array[index] = array[i];
+//            array[i] = temp;
+//        }
+//
+//    }
+}
